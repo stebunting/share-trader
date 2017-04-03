2b6a92946c2dace5acf922cdf25463d8b92822ba
Stephen Bunting <stebunting@gmail.com>
2017-04-03 12:42:07 +0200

    Updated database

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Secret settings
+mysqlhost = 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+mysqldb = 'gozfnz9kvngo4au2'
+mysqluser = 'ykauce7h3ec7y0ns'
+mysqlpassword = 'glab3ttiyo9awnk9'
+secretkey = '%\xd1/\xc3\xdb2\xc1\x92#\xd5\xab\xfaq\x94\xd2\xdc\x81\x7f\xb6\xd2(i\x81f'
