fd3d00680b7b055045cb0b2da6e8a26359722415
Stephen Bunting <stebunting@gmail.com>
2017-04-03 14:31:10 +0200

    Added settings

---
+++ settings.py
@@ -0,0 +1,5 @@
+mysqlhost = 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+mysqldb = 'gozfnz9kvngo4au2'
+mysqluser = 'ykauce7h3ec7y0ns'
+mysqlpassword = 'glab3ttiyo9awnk9'
+secretkey = "b'\xa6~\xc2y\xc0an}I\x91ln]\xb7\xb6)\xdd\xcdO\xccQ;\xa2c'"
