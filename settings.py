1b3c21a61db50e12f1b234b031811bcc5655ae61
Stephen Bunting <stebunting@gmail.com>
2017-04-03 22:48:43 +0200

    Removed settings

--- settings.py
+++
@@ -1,8 +0,0 @@
-mysqlhost = 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-mysqldb = 'gozfnz9kvngo4au2'
-mysqluser = 'ykauce7h3ec7y0ns'
-mysqlpassword = 'glab3ttiyo9awnk9'
-secretkey = "b'\xa6~\xc2y\xc0an}I\x91ln]\xb7\xb6)\xdd\xcdO\xccQ;\xa2c'"
-loc = 'en_GB'
-gmailuser = 'stebunting@gmail.com'
-gmailpassword = '(m1crowav3)'
